b0f6760871467bf578e7b2fcaaf101143cb30808
Mst Rinky Akter <mstrinkya@gmail.com>
2023-04-13 14:18:31 +0600

    use enent bubble to create calculator and clear

--- pin-matcher-solution-main/js/app.js
+++ pin-matcher-solution-main/js/app.js
@@ -25,15 +25,46 @@ const typeNumberField = document.getElementById('typed-numbers');
 document.getElementById('calculator').addEventListener('click',function(event){
     // console.log('numbers clicked');
     const number = event.target.innerText;
+    const previousTypedNumber = typeNumberField.value;
+
     if(isNaN(number)){
         if(number === 'C'){
               typeNumberField.value = '';
         }
-        console.log(number);
+        // console.log(number);
+    }
+    else if (number === '<'){
+        // console.log(previousTypedNumber);
+        const digits = previousTypedNumber.split('');
+        digits.pop();
+        const remainingDigits = digits.join('');
+        typeNumberField.value = remainingDigits;
     }
     else{
-       const previousTypedNumber = typeNumberField.value;
         const newTypedNumber = previousTypedNumber + number;
         typeNumberField.value = newTypedNumber;
+    }
+})
+
+document.getElementById('verify-pin').addEventListener('click',function(){
+    // console.log('verify button clicked');
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin= displayPinField.value;
+
+    const typedNumberField = document.getElementById('typed-numbers');
+    const typedNumber = typedNumberField.value;
+
+    const successPin = document.getElementById('pin-success');
+    const failurePin = document.getElementById('pin-failure');
+
+    if(typedNumber === currentPin){
+        
+        successPin.style.display = 'block';
+        failurePin.style.display = 'none';
+    }
+    else{
+      
+        failurePin.style.display = 'block';
+        successPin.style.display = 'none'
     }
 })
